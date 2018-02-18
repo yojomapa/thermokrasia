683415fc436c0379ccda8c8f2a4f0b3afee39a43
Jorge Palacio <jorge.palacio@globant.com>
2018-02-17 23:53:08 -0500

    Change signature of method TemperatureResource.save()

--- src/main/java/si/primorska/resources/TemperatureResource.java
+++ src/main/java/si/primorska/resources/TemperatureResource.java
@@ -1,13 +1,17 @@
 package si.primorska.resources;
 
-import javax.inject.Inject;
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import si.primorska.api.Temperature;
 import si.primorska.db.TemperatureDao;
-import si.primorska.service.TemperatureService;
 
 @Path("/temperature")
 public class TemperatureResource {
@@ -19,7 +23,14 @@ public class TemperatureResource {
   }
 
   @POST
-  public Response save(Temperature temperature) {
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response save(@NotNull @QueryParam("sensorId") String sensorId, @NotNull @QueryParam("temp") Double temp) {
+
+    Temperature temperature = new Temperature();
+    temperature.setSensedDate(LocalDateTime.now());
+    temperature.setSensorId(sensorId);
+    temperature.setTemp(temp);
+
     temperatureDao.insert(temperature);
     return Response.ok().build();
   }
